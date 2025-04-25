ea89e63ad47d0a09b4a74aff9bf40b29a0aa1394
unknown <pikinleon39@gmail.com>
2025-04-25 09:15:59 +0100

    update review api

--- Certify/src/Pages/Review.jsx
+++ Certify/src/Pages/Review.jsx
@@ -59,7 +59,7 @@ const Review = () => {
             >
                 {review.user?.photoUrl ? (
                 <img
-                  src={`https://certisfy.onrender.com/api${review.user.photoUrl}`}
+                  src={`https://certisfy.onrender.com${review.user.photoUrl}`}
                   alt={review.user.fullName || "User"}
                   className="h-16 w-16 rounded-full object-cover"
                   onError={(e) => {
